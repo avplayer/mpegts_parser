789921ec2c1845722c3eb0c88776c547bf0c050f
jack <jack.wgm@gmail.com>
2016-12-27 22:44:32 +0800

    update parser
    Signed-off-by: jack <jack.wgm@gmail.com>

--- main.cpp
+++ main.cpp
@@ -4,16 +4,23 @@
 #include <iostream>
 #include <boost/asio/streambuf.hpp>
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
 	FILE* fp = fopen(argv[1], "r+b");
 	if (!fp)
 		return -1;
+
 	util::mpegts_parser p;
 	boost::asio::streambuf buf;
 	int vc = 0;
 	int sc = 0;
 	int offset = 0;
+	int64_t pts = 0, dts = 0;
+	bool vknown_type = false;
+	bool aknown_type = false;
+
 	while (!feof(fp)) {
+
 		if (buf.size() < 188) {
 			auto pre = boost::asio::buffer_cast<void*>(buf.prepare(188 * 1000));
 			auto sz = fread(pre, 1, 188 * 1000, fp);
@@ -29,6 +36,20 @@ int main(int argc, char** argv) {
 			} else {
 				buf.consume(188);
 			}
+			if (info.is_video_ && !vknown_type) {
+				std::string s = p.stream_name(info.pid_);
+				if (!s.empty() && !vknown_type) {
+					vknown_type = true;
+					std::cout << "pid " << info.pid_ << " stream type: " << s << std::endl;
+				}
+			}
+			if (info.is_audio_ && !aknown_type) {
+				std::string s = p.stream_name(info.pid_);
+				if (!s.empty() && !aknown_type) {
+					aknown_type = true;
+					std::cout << "pid " << info.pid_ << " stream type: " << s << std::endl;
+				}
+			}
 			if (info.type_ == util::mpegts_info::idr && info.is_video_)
 				vc++;
 			if (info.start_ && info.is_video_) {
@@ -36,8 +57,10 @@ int main(int argc, char** argv) {
 				std::cout << "pos=" << offset << std::endl;
 			}
 
-			if (!suc) offset += 1;
-			else offset += 188;
+			if (!suc)
+				offset += 1;
+			else
+				offset += 188;
 		}
 	}
 	fclose(fp);
